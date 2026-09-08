d458af4961ec7cdc981b1420fc7bf51036c0fbb2
Suman Karmakar <karmakarsuman12138@gmail.com>
2026-09-08 20:22:44 +0530

    add

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const server = http.createServer((req, res) => {
                 res.write('Error loading Home Page'); 
                 return res.end(); 
             }
-            res.write(data); // this is the data
+            res.write(data); // this is the dat
             return res.end();
         });
     }
